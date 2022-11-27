9e5233e0fc38448e11ba7bde193ece05f02ab129
naps&snacks <j.antoniogarcia1130@gmail.com>
2022-11-26 21:45:06 -0600

    logic added to database and install files

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,17 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.initialPrompt = event;
+    butInstall.classList.toggle();
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const prompt = window.initialPrompt;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.initialPrompt = null;
+});
